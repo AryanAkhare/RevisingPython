34d35f204dcdbb89442762838550eb8e35a0f937
Aryan <aryanparas2004@gmail.com>
2025-03-15 14:39:30 +0530

    Loops

--- Loops.py
+++ Loops.py
@@ -1,2 +1,37 @@
-for i in range(0,5):
+# #for loop
+# for i in range(0,5):
+#     print(i)
+
+# name=["dave","lisa","john"]
+# # for x in name:
+# #     print(x)
+
+# # for x in "Mississippi":
+# #     print(x)
+
+# for x in name:
+#     if x=="lisa":
+#         break
+#     print(x)
+
+# for x in name:
+#     if x=="lisa":
+#         continue
+#     print(x) #SKIPS LISA
+
+# for i in range(2,4):
+#     print(i)
+
+for i in range(0,101,5):
     print(i)
+names2=["dave","Sara","john"]
+actions=["codes","eats","sleeps"]
+
+for x in names2:
+    for i in actions:
+        print(x+" "+i+".")
+# #while loop
+# value=1
+# while value<10:
+#     print(value)
+#     value=value+1
